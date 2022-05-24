18a2fb86b0b36f5a9f53ac13159ac50c2feb8cd6
Alexandre Lam <alexandrelam@outlook.com>
2022-05-24 21:33:25 +0200

    feat: migrate to express

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,17 +1,43 @@
-import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './app/typeDefs';
 import { resolvers } from './app/resolvers';
 import mongoose from 'mongoose';
 
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+
 async function startApolloServer(typeDefs, resolvers) {
   await mongoose.connect('mongodb://localhost:27017/blindtube');
 
+  // Required logic for integrating with Express
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  // Same ApolloServer initialization as before, plus the drain plugin.
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    csrfPrevention: true,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-  const { url } = await server.listen();
-  console.log(`🚀 Server ready at ${url}`);
+
+  // More required logic for integrating with Express
+  await server.start();
+  server.applyMiddleware({
+    app,
+
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: '/',
+  });
+
+  // Modified server startup
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
 startApolloServer(typeDefs, resolvers);
