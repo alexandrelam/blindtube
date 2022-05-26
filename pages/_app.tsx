eeb85ff592da83555c0c36602d4082dd293b6079
Alexandre Lam <alexandrelam@outlook.com>
2022-05-26 13:07:58 +0200

    background image blur

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { styled } from "@mui/system";
+
+const BackgroundImage = styled("div")({
+  backgroundImage: `url("blindtubebg.jpeg")`,
+  height: "100vh",
+  width: "100vw",
+});
+
+const BackgroundBlur = styled("div")({
+  backgroundColor: "rgba(0, 0, 0, 0.4)",
+  backdropFilter: "blur(200px)",
+  height: "100vh",
+  width: "100vw",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <BackgroundImage>
+      <BackgroundBlur>
+        <Component {...pageProps} />
+      </BackgroundBlur>
+    </BackgroundImage>
+  );
 }
 
 export default MyApp;
